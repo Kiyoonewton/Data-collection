e52bb9f5451c85663f5b67c44a4d35dd26c08bd3
kiyoonewton <kiyoonewton41@gmail.com>
2021-07-09 16:02:02 +0100

    make it better

--- index.js
+++ index.js
@@ -173,7 +173,7 @@ function setup(hello) {
     // }
   };
 }
-var botMatches = 67958;
+var botMatches = 67959;
 setInterval(() => {
   const newYears = "1 Jan 2022";
   var future = new Date(newYears);
